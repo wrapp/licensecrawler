430a6d52f24d4d4568d3f9da25e6aa5d39a2bbf8
Joakim Lundborg <joakim.lundborg@gmail.com>
2018-11-22 20:04:45 +0100

    Follow rel:next to get all the repos

--- clone.sh
+++ clone.sh
@@ -1,1 +1,7 @@
-curl -s "https://$GITHUB_API_TOKEN:@api.github.com/orgs/wrapp/repos?per_page=200" | jq .[].ssh_url| xargs -n 1 git clone
+url="https://api.github.com/orgs/wrapp/repos"
+
+while [ -n "$url" ]; do
+    curl $url --user $GITHUB_API_TOKEN: | jq .[].ssh_url| xargs -n 1 git clone
+    url=$( curl --user $GITHUB_API_TOKEN: -I $url | grep -oP '(?<=<)([^<]*)(?=>; rel="next")' )
+    echo $url
+done
